21bd38db62940bf019553be7dd2f3e8b219948dc
PatriciaDivi <patriciavianad@gmail.com>
2024-05-29 22:20:20 -0300

    feat:

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Product } from '@/data/types/products'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await apiFetch('/products', {
@@ -26,41 +27,14 @@ export default async function Home() {
   // console.log('---->', highLightedProduct)
 
   return (
-    <div className="grid max-h-[710px] grid-cols-9 grid-rows-6 gap-6">
-      <Link
-        href={`/product/${highLightedProduct.slug}`}
-        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
-      >
-        <Image
-          src={highLightedProduct.image}
-          className="group-hover:scale-105 transition-transform duration-500"
-          width={860}
-          height={860}
-          quality={100}
-          alt=""
-        />
-
-        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">{highLightedProduct.title}</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            {highLightedProduct.price.toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              maximumFractionDigits: 0,
-              minimumFractionDigits: 0,
-            })}
-          </span>
-        </div>
-      </Link>
-
-      {otherProducts.map((product) => (
+    <Suspense>
+      <div className="grid max-h-[710px] grid-cols-9 grid-rows-6 gap-6">
         <Link
-          key={product.id}
-          href={`/product/${product.slug}`}
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
+          href={`/product/${highLightedProduct.slug}`}
+          className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
         >
           <Image
-            src={product.image}
+            src={highLightedProduct.image}
             className="group-hover:scale-105 transition-transform duration-500"
             width={860}
             height={860}
@@ -68,10 +42,10 @@ export default async function Home() {
             alt=""
           />
 
-          <div className="absolute sm:bottom-10 sm:right-2 lg:bottom-10 lg:right-10 h-12 flex items-center gap-2 sm:max-w-[220px] lg:max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">{product.title}</span>
+          <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">{highLightedProduct.title}</span>
             <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {product.price.toLocaleString('pt-BR', {
+              {highLightedProduct.price.toLocaleString('pt-BR', {
                 style: 'currency',
                 currency: 'BRL',
                 maximumFractionDigits: 0,
@@ -80,7 +54,36 @@ export default async function Home() {
             </span>
           </div>
         </Link>
-      ))}
-    </div>
+
+        {otherProducts.map((product) => (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
+          >
+            <Image
+              src={product.image}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={860}
+              height={860}
+              quality={100}
+              alt=""
+            />
+
+            <div className="absolute sm:bottom-10 sm:right-2 lg:bottom-10 lg:right-10 h-12 flex items-center gap-2 sm:max-w-[220px] lg:max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Suspense>
   )
 }
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Product } from '@/data/types/products'
 import { Metadata } from 'next'
 import { z } from 'zod'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 interface ProductProps {
   params: {
@@ -55,75 +56,77 @@ export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
 
   return (
-    <div className="relative grid max-h-[860px] grid-cols-3">
-      <div className="col-span-2 overflow-hidden">
-        <Image
-          src={product.image}
-          alt={product.title}
-          width={800}
-          height={800}
-          quality={100}
-        />
-      </div>
-
-      <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3x1 font-bold leading-tight">{product.title}</h1>
-
-        <p className="mt-2 leading-relaxed text-zinc-45">
-          {product.description}
-        </p>
-
-        <div className="mt-8 flex items-center gap-3">
-          <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            {product.price.toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              maximumFractionDigits: 0,
-              minimumFractionDigits: 0,
-            })}
-          </span>
-          <span>
-            Em 12x s/ juros de{' '}
-            {(product.price / 12).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            })}
-          </span>
+    <Suspense>
+      <div className="relative grid max-h-[860px] grid-cols-3">
+        <div className="col-span-2 overflow-hidden">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={800}
+            height={800}
+            quality={100}
+          />
         </div>
 
-        <div className="mt-8 space-y-4">
-          <span className="block font-semibold">Tamanhos</span>
+        <div className="flex flex-col justify-center px-12">
+          <h1 className="text-3x1 font-bold leading-tight">{product.title}</h1>
 
-          <div className="flex gap-2">
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              P
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              M
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              G
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              GG
-            </button>
+          <p className="mt-2 leading-relaxed text-zinc-45">
+            {product.description}
+          </p>
+
+          <div className="mt-8 flex items-center gap-3">
+            <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+              {product.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              })}
+            </span>
+            <span>
+              Em 12x s/ juros de{' '}
+              {(product.price / 12).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
           </div>
-        </div>
 
-        <AddToCartButton productId={product.id} />
+          <div className="mt-8 space-y-4">
+            <span className="block font-semibold">Tamanhos</span>
+
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              >
+                P
+              </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              >
+                M
+              </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              >
+                G
+              </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              >
+                GG
+              </button>
+            </div>
+          </div>
+
+          <AddToCartButton productId={product.id} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
